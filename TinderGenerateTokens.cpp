0e40e9c75c67a5fd0ad7dce61bd042af7dbd5a9d
ReverseAndBot <gh@reverseandbot.com>
2024-08-25 12:47:21 +0200

    Create TinderGenerateTokens.cpp

---
+++ TinderGenerateTokens.cpp
@@ -0,0 +1,38 @@
+//
+// TinderGenerateTokens.cpp
+//
+// $Id: ... $
+//
+// This class generates the Tinder signature variables
+//
+// Requirements: no external API, no external service
+//
+// Copyright (C) 2023 - 2024 by ReverseEngineering & Partners, West Europe (GitHub: github.com/ReverseAndBot - E-Mail: info@reverseandbot.com - Telegram: ReverseEngineeringBHW)
+
+
+
+#include "TinderGenerateTokens.h"
+
+
+
+std::string TinderGenerateTokens::generateHTTPHeaderSignature (std::string strJob) {
+
+   std::string strHTTPHeaderSignature = "";
+
+
+   if (strJob == "send-chat-message") {
+
+      // Code removed due to privacy/security reasons
+      // GitHub: https://github.com/ReverseAndBot - Telegram: ReverseEngineeringBHW - Email: info@reverseandbot.com
+
+   } else if (strJob == "swipe-right") {
+
+      // Code removed due to privacy/security reasons
+      // GitHub: https://github.com/ReverseAndBot - Telegram: ReverseEngineeringBHW - Email: info@reverseandbot.com
+
+   }
+
+
+   return strHTTPHeaderSignature;
+
+}
